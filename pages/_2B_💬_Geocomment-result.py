54f404153c0b41880bbefc97e8cd22823ce51923
Quang Huy <kaheetonaa@gmail.com>
2024-11-29 14:18:46 +0100

    hello

---
+++ pages/_2B_💬_Geocomment-result.py
@@ -0,0 +1,102 @@
+import streamlit as st
+from pymongo import MongoClient
+import pandas as pd
+import geopandas as gpd
+from shapely import wkt
+import folium
+from folium.features import GeoJsonPopup
+from streamlit_folium import st_folium
+
+st.set_page_config(layout="wide")
+
+@st.cache_resource
+def init_connection():
+    return MongoClient("mongodb+srv://kuquanghuy:quanghuy123456@cluster0.6mzug.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+
+client = init_connection()
+
+db=client['PoliMappers01']
+collection=db['PoliMappers01']
+result_polygon=pd.DataFrame(list(collection.find().sort("_id", -1).limit(30)))
+
+result_polygon['Polygon']=gpd.GeoSeries.from_wkt(result_polygon['bounds'])
+result_polygon['Point']=gpd.GeoSeries.from_wkt(result_polygon['center'])
+result_polygon=gpd.GeoDataFrame(result_polygon,geometry=result_polygon['Polygon']).set_crs(epsg=4326)
+
+
+
+st.markdown("""
+
+<style>
+    @import url('https://fonts.googleapis.com/css?family=Comfortaa:wght@100&display=swap'); 
+
+    html, body, h1,h2,h3,p{
+        font-family: 'Comfortaa', sans-serif; 
+        
+    }
+    div[data-baseweb="select"] > div {
+    background-color: #62cbec;
+            color:white;
+            font-family: 'Comfortaa', sans-serif;
+    }
+
+    body{
+        font-size: 18px;
+    }
+
+    [role=radiogroup]{
+        gap: 1rem;
+    }
+    h1 {
+        text-align: center
+    }
+    h2 {
+        text-align: center
+    }
+    h3 {
+        text-align: center
+    }
+    
+    div[data-testid='stAppViewBlockContainer']{
+        background-color: #62cbec10;
+    }
+            .center {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+        width:200px;
+}
+</style>
+""", unsafe_allow_html=True)
+if st.button('Refresh'):
+    #This would empty everything inside the container
+    st.empty()
+
+map = folium.Map(
+    location=[0,0], zoom_start=5, max_zoom=21)
+for _, r in result_polygon.iterrows():
+    # Without simplifying the representation of each borough,
+    # the map might not be displayed
+    pol = gpd.GeoSeries(r["Polygon"])#.simplify(tolerance=0.001)
+    pol_j = pol.to_json()
+    pol_j = folium.GeoJson(data=pol_j, style_function=lambda x: {"fillColor": "red","fillOpacity":"5%","color":"red"})
+    
+    poi_j=gpd.GeoSeries(r["Point"]).to_json()
+    poi_j= folium.GeoJson(data=poi_j
+                        ,marker=folium.Marker(icon=folium.Icon(icon='earth-americas',prefix='fa'))
+                        ,style_function=lambda x: {"markerColor": "red"}
+                        ,zoom_on_click=True)
+    folium.Popup(r["comment"]).add_to(poi_j)
+    pol_j.add_to(map)
+    poi_j.add_to(map)
+
+map.fit_bounds(map.get_bounds(), padding=(30, 30))
+
+st_map= st_folium(
+    map,
+    width='100%',
+    height=600,
+    returned_objects=[]
+)
+
+
---
+++ pages/_2_💬_Geocomment.py
@@ -0,0 +1,132 @@
+import streamlit as st
+import requests
+import geopandas as gpd
+import folium
+from folium.features import GeoJsonPopup
+from streamlit_folium import st_folium
+from pymongo import MongoClient
+
+
+st.set_page_config(layout="wide")
+
+@st.cache_resource
+def init_connection():
+    return MongoClient("mongodb+srv://kuquanghuy:quanghuy123456@cluster0.6mzug.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+
+client = init_connection()
+
+db=client['PoliMappers01']
+collection=db['PoliMappers01']
+
+
+st.markdown("""
+
+<style>
+    @import url('https://fonts.googleapis.com/css?family=Comfortaa:wght@100&display=swap'); 
+
+    html, body, h1,h2,h3,p{
+        font-family: 'Comfortaa', sans-serif; 
+        
+    }
+    div[data-baseweb="select"] > div {
+    background-color: #62cbec10;
+            color:black;
+            font-family: 'Comfortaa', sans-serif;
+    }
+
+    body{
+        font-size: 18px;
+    }
+
+    [role=radiogroup]{
+        gap: 1rem;
+    }
+    h1 {
+        text-align: center
+    }
+    h2 {
+        text-align: center
+    }
+    h3 {
+        text-align: center
+    }
+    
+    div[data-testid='stAppViewBlockContainer']{
+        background-color: #62cbec10;
+    }
+            .center {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+        width:200px;
+}
+</style>
+""", unsafe_allow_html=True)
+
+popup = GeoJsonPopup(
+    fields=['projecId-str']
+)
+def load_json_from_url(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an exception for error status codes
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        print(f"Error making request: {e}")
+        raise
+    except ValueError as e:
+        print(f"Invalid JSON response: {e}")
+        raise
+
+def style_function(feature):
+    props = feature.get('properties')
+    markup = f"""
+            <div style="width: 20px;
+                        height: 20px;
+                        border: 1px solid red;
+                        border-radius: 10px;
+                        background-color: #ff000030;">
+            </div>
+        </div>
+    """
+    return {"html": markup}
+
+@st.fragment
+def drawMap(location,zoom):
+    map = folium.Map(
+    location=location, zoom_start=zoom, max_zoom=21)
+    st_map= st_folium(
+    map,
+    width='100%',
+    height=400
+    )
+    st.session_state.location=[st_map['center']['lat'],st_map['center']['lng']]
+    st.session_state.zoom=st_map['zoom']
+    st.session_state.bounds=st_map['bounds']
+
+#form
+comment = st.text_input("Write your experience on the textbox, zoom the map to the area you have a mapping experience", "")
+
+if st.button('Submit'):
+    if comment!="":
+        st.write("✅You submit the area at coordinate",str(st.session_state.location),'at the zoom of',str(st.session_state.zoom),'with the story of', comment)
+        post={'bounds':'POLYGON (('+str(st.session_state.bounds['_southWest']['lng'])+' '+str(st.session_state.bounds['_southWest']['lat'])+','+str(st.session_state.bounds['_southWest']['lng'])+' '+str(st.session_state.bounds['_northEast']['lat'])+','+str(st.session_state.bounds['_northEast']['lng'])+' '+str(st.session_state.bounds['_northEast']['lat'])+','+str(st.session_state.bounds['_northEast']['lng'])+' '+str(st.session_state.bounds['_southWest']['lat'])+','+str(st.session_state.bounds['_southWest']['lng'])+' '+str(st.session_state.bounds['_southWest']['lat'])+'))','comment':comment,'center':'POINT ('+str(st.session_state.location[1])+' '+str(st.session_state.location[0])+')','zoom':st.session_state.zoom}
+        collection.insert_one(post)
+
+#referencing
+
+
+if 'location' not in st.session_state:
+    st.session_state.location = [0, 0]
+if 'zoom' not in st.session_state:
+    st.session_state.zoom = 5
+#map
+a=drawMap(st.session_state.location,st.session_state.zoom)
+
+
+
+
+
+
+
+
