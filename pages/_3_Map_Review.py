5147edd057394091cfa7afb0738e3b36ecb7a905
kaheetonaa <61049296+kaheetonaa@users.noreply.github.com>
2025-01-22 12:30:47 +0100

    test

--- pages/_3_Map_Review.py
+++ pages/_3_Map_Review.py
@@ -61,13 +61,24 @@ START_ZOOM = 16
 
 features = gpd.GeoDataFrame.from_features(osm_data).set_crs(epsg=4326)
 
+id=[]
+user=[]
+shape=[]
+tag=[]
+version=[]
+
+
 st.write(features)
 
 for o in osmium.FileProcessor("https://api06.dev.openstreetmap.org/api/0.6/map?bbox=9.21734%2C45.47109%2C9.23813%2C45.48607").with_areas().with_locations().with_filter(osmium.filter.GeoInterfaceFilter()):
     if o.is_way():
+        id+=o.id
+        user+=o.user
+        shape+=shape(o.__geo_interface__['geometry'])
+        version+=o.version
         st.write(o.id,o.user,o.version,o.tags,shape(o.__geo_interface__['geometry']))
 
-
+st.write(id)
 
 building=features[features.geometry.type=='MultiPolygon'][features.building.notnull()]
 #highway=features[features.geometry.type=='LineString'][features.highway.notnull()]
