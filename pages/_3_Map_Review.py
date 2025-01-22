0d59ecd8d2ecb18fcdecf410eabd91873c814567
kaheetonaa <61049296+kaheetonaa@users.noreply.github.com>
2025-01-22 12:07:07 +0100

    test

--- pages/_3_Map_Review.py
+++ pages/_3_Map_Review.py
@@ -52,8 +52,12 @@ st.markdown("""
 </style>
 """, unsafe_allow_html=True)
 
+@st.cache_data
+def fetch_and_clean_data(url):
+    # Fetch data from URL here, and then clean it up.
+    return data
 
-osm_input=osmium.FileProcessor("https://api06.dev.openstreetmap.org/api/0.6/map?bbox=9.21734%2C45.47109%2C9.23813%2C45.48607").with_filter(osmium.filter.EntityFilter(osmium.osm.WAY))
+osm_input=osmium.FileProcessor(fetch_and_clean_data("https://api06.dev.openstreetmap.org/api/0.6/map?bbox=9.21734%2C45.47109%2C9.23813%2C45.48607")).with_filter(osmium.filter.EntityFilter(osmium.osm.WAY))
 
 osm_data= osm_input.with_areas().with_filter(osmium.filter.GeoInterfaceFilter())
 START_LOCATION = [9.227909,45.478059]
