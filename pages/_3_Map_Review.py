16a0f0052ae2cd33cea5c8076dba8130ca9ab38b
kaheetonaa <61049296+kaheetonaa@users.noreply.github.com>
2025-01-22 13:46:03 +0100

    test

--- pages/_3_Map_Review.py
+++ pages/_3_Map_Review.py
@@ -73,12 +73,12 @@ for o in osmium.FileProcessor("https://api06.dev.openstreetmap.org/api/0.6/map?b
     if o.is_way():
         id.append(str(o.id))
         user.append(o.user)
-        geom.append(o.__geo_interface__['geometry'])
+        geom.append(shape(o.__geo_interface__['geometry']))
         version.append(o.version)
         tag.append(str(o.tags))
         #st.write(o.id,o.user,o.version,o.tags,shape(o.__geo_interface__['geometry']))
 
-osm_gpd=gpd.GeoDataFrame({'id':id,'user':user,'tag':tag,'version':version},geometry=geom)
+osm_gpd=gpd.GeoDataFrame({'id':id,'user':user,'tag':tag,'version':version},geometry=gpd.GeoSeries(geom))
 osm_gpd
 
 #osm_gpd['id']=id
