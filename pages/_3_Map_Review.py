c5f75949c03d3a8c3efb915133074f7b284365b0
kaheetonaa <61049296+kaheetonaa@users.noreply.github.com>
2025-01-22 13:38:31 +0100

    test

--- pages/_3_Map_Review.py
+++ pages/_3_Map_Review.py
@@ -87,7 +87,7 @@ features
 #osm_gpd['tag']=tag
 #osm_gpd['version']=version
 
-building=features[features.geometry.type=='MultiPolygon'][features.building.notnull()]
+building=features[features.geometry.type=='MultiPolygon'][features['tag']=='building=yes']
 #highway=features[features.geometry.type=='LineString'][features.highway.notnull()]
 
 building_style = {"fillColor": "red", "fillOpacity": 0.2,"color":"red"}
