617f7367a50a6374b7002e2e67b6feb69ca161be
kaheetonaa <61049296+kaheetonaa@users.noreply.github.com>
2025-01-22 11:39:27 +0100

    print

--- pages/_3_Map_Review.py
+++ pages/_3_Map_Review.py
@@ -56,7 +56,7 @@ osm_data= osmium.FileProcessor("https://api06.dev.openstreetmap.org/api/0.6/map?
 START_LOCATION = [9.227909,45.478059]
 START_ZOOM = 16
 
-st.write(osm_data)
+print(osm_data)
 
 features = gpd.GeoDataFrame.from_features(osm_data).set_crs(epsg=4326)
 
