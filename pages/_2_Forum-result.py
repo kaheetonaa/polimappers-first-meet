f07c64f72d05dd285cd9f6e0e706cd2c2f95c23e
kaheetonaa <61049296+kaheetonaa@users.noreply.github.com>
2026-04-22 08:55:37 +0200

    forum

---
+++ pages/_2_Forum-result.py
@@ -0,0 +1,97 @@
+import streamlit as st
+import pandas as pd
+from pymongo import MongoClient
+
+st.set_page_config(layout="wide")
+st.markdown("""
+
+<style>
+    @import url('https://fonts.googleapis.com/css?family=Comfortaa:wght@100&display=swap'); 
+
+    html, body, h1,h2,h3,p{
+        font-family: 'Comfortaa', sans-serif; 
+        
+    }
+    div[data-baseweb="select"] > div {
+    background-color: #62cbec10;
+            color:white;
+            font-family: 'Comfortaa', sans-serif;
+    }
+
+    body{
+        font-size: 18px;
+        gap: 20px;
+    }
+
+    [role=radiogroup]{
+        gap: 1rem;
+    }
+    h1 {
+        text-align: center
+    }
+    h2 {
+        text-align: center
+    }
+    h3 {
+        text-align: center
+    }
+    
+    div[data-testid='stAppViewBlockContainer']{
+        background-color: #62cbec10;
+    }
+            .center {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+        width:200px;
+            }
+            
+    .bubble {
+  --r: 1em;  /* the radius */
+  --t: 1.5em; /* the size of the tail */
+  
+  max-width: 400px;
+  padding: 1em;
+  border-inline: var(--t) solid #0000;
+  border-radius: calc(var(--r) + var(--t))/var(--r);
+  mask: 
+    radial-gradient(100% 100% at var(--_p) 0,#0000 99%,#000 102%) 
+      var(--_p) 100%/var(--t) var(--t) no-repeat,
+    linear-gradient(#000 0 0) padding-box;
+  background: #62cbec;
+  color: #fff;
+}
+.left {
+  --_p: 0;
+  border-bottom-left-radius: 0 0;
+  place-self: start;
+}
+.right {
+  --_p: 100%;
+  border-bottom-right-radius: 0 0;
+  place-self: end;
+}
+</style>
+""", unsafe_allow_html=True)
+@st.cache_resource
+def init_connection():
+    return MongoClient("mongodb+srv://kuquanghuy:quanghuy123456@cluster0.6mzug.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+
+client = init_connection()
+
+db=client['rovereto']
+collection=db['rovereto_forum']
+result=pd.DataFrame(list(collection.find().sort("_id", -1)))
+
+st.title('Domanda per la mappatura del territorio in caso di calamità ?')
+
+st.write(result)
+
+for i in range(len(result)):
+  comment=result['comment'][i]
+  if i % 2:
+    st.markdown("""<div class="bubble right">"""+comment+"""</div>
+    <div class="right">"""+result['school'][i]+"""</div>""",unsafe_allow_html=True)
+  else:
+    st.markdown("""<div class="bubble left">"""+comment+"""</div>
+    <div class="left">"""+result['school'][i]+"""</div>""",unsafe_allow_html=True)
---
+++ pages/_2_Forum.py
@@ -0,0 +1,84 @@
+import streamlit as st
+from pymongo import MongoClient
+st.set_page_config(
+  page_title="🌐 EuthMappers quizz",
+  page_icon="❓",
+  layout="wide"
+)
+st.markdown("""
+<style>
+    @import url('https://fonts.googleapis.com/css?family=Comfortaa:wght@100&display=swap'); 
+
+    html, body, h1,h2,h3,p{
+        font-family: 'Comfortaa', sans-serif; 
+        
+    }
+    div[data-baseweb="select"] > div {
+    background-color: #62cbec10;
+            color:black;
+            font-family: 'Comfortaa', sans-serif;
+    }
+
+    body{
+        font-size: 18px;
+        gap: 20px;
+    }
+
+    [role=radiogroup]{
+        gap: 1rem;
+    }
+    h1 {
+        text-align: center
+    }
+    h2 {
+        text-align: center
+    }
+    h3 {
+        text-align: center
+    }
+    
+    div[data-testid='stVerticalBlockBorderWrapper']{
+        background-color: #ffffffC0;
+    }
+            .center {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+        width:200px;
+            }
+</style>
+""", unsafe_allow_html=True)
+
+@st.cache_resource
+def init_connection():
+    return MongoClient("mongodb+srv://kuquanghuy:quanghuy123456@cluster0.6mzug.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+client = init_connection()
+
+db=client['EuthMappers_Forum_241126']
+collection=db['EuthMappers_Forum_241126']
+st.title('What is Sustainable Development ?')
+container1 = st.container()
+def submit_answer():
+        if (comment!=""):
+                st.write(comment)
+                post={'comment':comment}
+                collection.insert_one(post)
+                st.session_state.answer_submitted=True
+                st.write(st.session_state.answer_submitted)
+        else:
+                st.warning('Nessuna domanda scritta!')
+with container1:
+    st.html("<img src='https://raw.githubusercontent.com/kaheetonaa/streamlit_quizz_template_euth/refs/heads/main/asset/logo.png' class='center'/>")
+    st.markdown(""" ___""")
+    if "answer_submitted" not in st.session_state:
+        comment = st.text_input("Domanda per la mappatura del territorio in caso di calamità ?", "")
+        st.button('Consegna', on_click=submit_answer)
+    else:
+        st.balloons()
+        st.write('Hai consegnato la domanda!')
+
+        
+
+
+
+
